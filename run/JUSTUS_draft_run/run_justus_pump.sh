560f9de4814996e6982889f90f040c6066c0b11e
MarcNairn <marcnairn@gmail.com>
2023-11-20 10:38:51 +0100

    temp push

--- run/JUSTUS_draft_run/run_justus_pump.sh
+++ run/JUSTUS_draft_run/run_justus_pump.sh
@@ -5,5 +5,4 @@
 #SBATCH --time=1-00:00:00
 #SBATCH --mem=0
 
-#export JULIA_DEPOT_PATH=/cluster/userdata/luigi/.julia
-julia run_parallel_snowden_pump.jl 50 $1
+julia run_parallel_justus_pump.jl 50 $1
