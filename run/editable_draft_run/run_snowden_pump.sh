19540f5e4b829fa688e3b938e95a002380911010
MarcNairn <marcnairn@gmail.com>
2023-11-20 10:25:28 +0100

    Renaming

--- run/editable_draft_run/run_snowden_pump.sh
+++ run/editable_draft_run/run_snowden_pump.sh
@@ -3,8 +3,7 @@
 #SBATCH --ntasks=50
 #SBATCH --cpus-per-task=1
 #SBATCH --time=1-00:00:00
-#SBATCH --partition=oneside
 #SBATCH --mem=0
 
 #export JULIA_DEPOT_PATH=/cluster/userdata/luigi/.julia
-julia run_parallel_snowden.jl 50 $1
+julia run_parallel_snowden_pump.jl 50 $1
