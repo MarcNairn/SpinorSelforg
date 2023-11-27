bdeb5e929995fb2f45b9962b415ca765a1e9486a
MarcNairn <marcnairn@gmail.com>
2023-11-27 11:21:02 +0100

    parse arguments to .jld in main julia call

--- run/JUSTUS_draft_run/run_parallel_justus_static.jl
+++ run/JUSTUS_draft_run/run_parallel_justus_static.jl
@@ -2,11 +2,12 @@ using ClusterManagers
 using Random
 
 np = parse(Int,ARGS[1])
+Sre = parse(Int, ARGS[2])
 
 include("../load.jl")
 include("../parameters_static.jl")
 
-Sre = Real(p.S₁)
+
 
 sim = many_trajectory_solver(p,saveat=10.0, seed=abs(rand(Int)), maxiters=Int(1.0e9))
-save_datal("~/SpinorSelforg/cluster_data/static_pump$(Sre)_$(p.tspan)_nt=$(np).jld2", sim)
+save_datal("sim_data_static_pump(S=$(Sre), t=800, $np).jld2", sim)
