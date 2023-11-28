cdad0d51db05e098483cb60f1b0dedd85705fafd
MarcNairn <marcnairn@gmail.com>
2023-11-28 12:02:09 +0100

    FIXED load.jl PATH (required to keep as is for cluster runs)

--- run/load.jl
+++ run/load.jl
@@ -1,4 +1,4 @@
 #using Pkg
 #Pkg.activate("..")
 using DifferentialEquations
-include("~/SpinorSelforg/src/selforg_core.jl")
+include("../src/selforg_core.jl")
--- sim_reader.jl
+++ sim_reader.jl
@@ -1,24 +1,12 @@
 #THIS SCRIPT EXTRACTS ALL SIMULATIONS STORED AS .JLD FILES INTO A SIMULATION ARRAY FROM WHICH WE MAY EXTRACT THE OBSERVABLE sim_data
 using JLD2
 
-include("src/custom_functions.jl")
+#include("src/custom_functions.jl")
 include("src/selforg_core.jl")
 
-#re-import in case struct System_p undefined when loading data
-struct System_p
-    U₁::Float64 
-    U₂::Float64
-    S₁::Complex{Float64}
-    S₂::Complex{Float64}
-    Δₑ::Float64
-    Δc::Float64
-    κ::Float64
-    temp::Float64
-    N::Int
-    tspan::Tuple{Float64,Float64}
-    N_MC::Int
-end
 
+
+#TO EXTRACT ALL SIMULATION DATA INTO A COMMON ARRAY
 function load_datalll(directory::AbstractString)
     sim = []
 
@@ -33,5 +21,5 @@ function load_datalll(directory::AbstractString)
     merge_sim(sim...)
 end
 
-sim_array = load_datalll("cluster_files/")
+sim_array = load_datalll("cluster_files/") #of type Vector{Sol} (or Array{Sol,1})
 
