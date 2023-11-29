f8d935f9da953d2ac3233966bd412c11b0394d2e
MarcNairn <marcnairn@gmail.com>
2023-11-29 12:50:11 +0100

    data sorter

--- src/sim_reader.jl
+++ src/sim_reader.jl
@@ -4,8 +4,6 @@ using JLD2
 
 include("selforg_core.jl")
 
-
-
 #TO EXTRACT ALL SIMULATION DATA INTO A COMMON ARRAY
 function load_datalll(directory::AbstractString)
     sim = []
@@ -21,14 +19,39 @@ function load_datalll(directory::AbstractString)
     merge_sim(sim...)
 end
 
-sim_array = load_datalll("../cluster_files/") #of type Vector{Sol} (or Array{Sol,1})
+#RUN THROUGH ALL SIM DATA, CATEGORISE IT IN BATCHES OF THE SAME PARAMETERS AND SAVE IT IN MANY TRAJECTORY FILES
+function sort_save_datal(directory::AbstractString, set_traj_number::Int) #take array of array of solutions, i.e. array of many trajectory simulations per entry
+    #set_traj_number to some integer we are aware should match with the desired number of trajectories 
 
-sorted_sims = split_sim_from_par(sim_array)
+    sims_to_sort =  load_datalll(directory)
+    sorted_sims = split_sim_from_par(sims_to_sort)
 
-#RUN THROUGH ALL SIM DATA AND CATEGORISE IT IN BATCHES OF THE SAME PARAMETERS
 
-for batch in sorted_sims 
-    g = Int(batch[1].p.S₁)
-    trajs = length(batch)
-    save_datal("../sorted_sims/S=$(g), Ntraj=$(trajs)-sim_data.jld2", batch)
+    for batch in sorted_sims 
+        g = Int(batch[1].p.S₁) #to sort by pumping strength
+        trajs = length(batch) #make sure all have same length otherwise discard current iteration
+
+        #Check for correct number of trajectories:
+        if trajs == set_traj_number
+            # Construct the directory path
+            target_directory = "sorted_sims"
+        
+            # Create the directory if it doesn't exist
+            isdir(target_directory) || mkdir(target_directory)
+            
+             # Construct the file path
+             file_path = joinpath(target_directory, "S=$(g), Ntraj=$(trajs)-sim_data.jld2")
+            
+             # Check if the file already exists
+            if !isfile(file_path)
+                # save data
+                save_datal(file_path, batch)
+                println("Creating file for g=$(g) and $(trajs) trajs in $target_directory")
+            else
+                println("File for g=$(g) and $(trajs) trajs already exists. Skipping...")
+            end
+        else
+            println("Skipping batch for g=$(g) and $(trajs) trajs as it does not have the required length.")
+        end
+    end
 end
